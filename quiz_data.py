dee2a8e455f93ca2a2efecd5716c4448234801c7
RUTIK VIKASRAO KAWALE <159055645+RutikVKawale@users.noreply.github.com>
2024-02-29 09:11:04 +0530

    Add files via upload

---
+++ quiz_data.py
@@ -0,0 +1,43 @@
+quiz_data = [
+    {
+        "question": "What is the capital of France?",
+        "choices": ["Paris", "London", "Berlin", "Madrid"],
+        "answer": "Paris"
+    },
+    {
+        "question": "What is the largest planet in our solar system?",
+        "choices": ["Jupiter", "Saturn", "Mars", "Earth"],
+        "answer": "Jupiter"
+    },
+    {
+        "question": "What is the chemical symbol for gold?",
+        "choices": ["Go", "Au", "Ag", "Gd"],
+        "answer": "Au"
+    },
+    {
+        "question": "Which country is known as the 'Land of the Rising Sun'?",
+        "choices": ["China", "Japan", "South Korea", "Thailand"],
+        "answer": "Japan"
+    },
+    {
+        "question": "What is the chemical symbol for silver?",
+        "choices": ["Go", "Au", "Ag", "Gd"],
+        "answer": "Ag"
+    },
+    {
+        "question": "who was the first President of India",
+        "choices": ["Tarak Mehta", "Mahatma Gandhi", "Sardar Valhabhai patel", "Dr.Rajendra Prasad"],
+        "answer": "Dr.Rajendra Prasad"
+    },
+    {
+        "question": "Smallest state of India is",
+        "choices": ["Goa", "Maharashtra", "Aasam", "Manipur"],
+        "answer": "Goa"
+    },
+    {
+        "question": "Gateway of India is located at",
+        "choices": ["Delhi", "Mumbai", "Goa", "Jaipur"],
+        "answer": "Mumbai"
+    }
+    # Add more questions here
+]
